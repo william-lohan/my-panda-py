4f84d5eecdacb4c6f15738bbf9934ec351272006
William Lohan <william.g.lohan@gmail.com>
2023-05-18 00:36:07 -0700

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.disableMouse()
+
+        properties = WindowProperties()
+        properties.setSize(1000, 750)
+        self.win.requestProperties(properties)
+
+game = Game()
+game.run()
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+setup(
+    name='mypanda',
+    options={
+        'build_apps': {
+            # Build asteroids.exe as a GUI application
+            'gui_apps': {
+                'asteroids': 'main.py',
+                'mypanda': 'filename.py',
+            },
+
+            # Set up output logging, important for GUI apps!
+            'log_filename': '$USER_APPDATA/Asteroids/output.log',
+            'log_append': False,
+
+            # Specify which files are included with the distribution
+            'include_patterns': [
+                '**/*.png',
+                '**/*.jpg',
+                '**/*.egg',
+
+                # Path to a specific file
+                'CREDITS.txt',
+
+                # All files in the assets/textures/ directory, but not in subdirectories
+                # (use ** instead of * if that is desirable)
+                'assets/textures/*',
+
+                # All files with the .jpg extension in any subdirectory under assets/,
+                # even if nested under multiple directories
+                'assets/**/*.jpg',
+
+                # A file with the .egg extension anywhere in the hierarchy
+                '**/*.egg',
+            ],
+
+            # Include the OpenGL renderer and OpenAL audio plug-in
+            'plugins': [
+                'pandagl',
+                'p3openal_audio',
+            ],
+
+            'platforms': ['manylinux2014_x86_64', 'macosx_10_9_x86_64', 'win_amd64'],
+        }
+    }
+)
